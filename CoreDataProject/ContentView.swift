80db1f9d201847fa6e2e6f9279427141a97edd78
Reed Harston <reed@caseirosoftware.com>
2023-12-14 17:13:46 -0800

    Initial Commit

---
+++ CoreDataProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Reed Harston on 12/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Reed Harston on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
